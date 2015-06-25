08673d6d10155b0e37ff49a02d898384565c4771
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-06-25 18:32:27 +0200

    Fix color naming and usage:
    "android:colorPrimary" is used for the action-/tool bar,
    while "android:colorPrimaryDark" is used for the status bar,
    so use the correct attribute- and color naming.
    
    "calculator_error_color" is the color used for the animation to the error state,
    and the status bar color turnes to this color,
    on normal state, the status bar turnes back to the default status bar color,
    so the animation to the normal state should use the default status bar color instead of the accent color.
    
    Conflicts:
    	res/values/styles.xml
    	src/com/android/calculator2/Calculator.java

--- src/com/android/calculator2/Calculator.java
+++ src/com/android/calculator2/Calculator.java
@@ -359,7 +359,7 @@ public class Calculator extends Activity
             } else if (mCurrentState != CalculatorState.RESULT) {
                 mFormulaText.setTextColor(getColor(R.color.display_formula_text_color));
                 mResultText.setTextColor(getColor(R.color.display_result_text_color));
-                getWindow().setStatusBarColor(getColor(R.color.calculator_primary_color));
+                getWindow().setStatusBarColor(getColor(R.color.calculator_primary_dark_color));
             }
 
             invalidateOptionsMenu();
@@ -736,7 +736,7 @@ public class Calculator extends Activity
         }
         cancelIfEvaluating(true);
         announceClearedForAccessibility();
-        reveal(mCurrentButton, R.color.calculator_accent_color, new AnimatorListenerAdapter() {
+        reveal(mCurrentButton, R.color.calculator_primary_dark_color, new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
                 mUnprocessedChars = null;
